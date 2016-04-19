4b0d8c0f529d4281f2b2edeff21faa40bb504796
Allen Boynton <aboynton@fullsail.edu>
2016-04-19 19:32:40 -0400

    Initial Commit to repo

---
+++ ABoynton_CE6/ABoynton_CE6/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ABoynton_CE6
+//
+//  Created by Allen Boynton on 4/19/16.
+//  Copyright © 2016 Full Sail. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
